9272b3635fd969e9f04c290ae428ec301064c1f4
id <msgexec@gmail.com>
2020-05-15 17:17:43 +0300

    feat(project): add RedisFlushDb func for invalidate all cache

--- cache.go
+++ cache.go
@@ -59,6 +59,10 @@ func (c *SyncCacheClient) UpdateRedisKey(cacheGroupName, key string) {
 	c.redis.Set(cacheGroupName+"_"+key, newUuid, 0)
 }
 
+func (c *SyncCacheClient) RedisFlushDb() {
+	c.redis.FlushDB()
+}
+
 func (c *SyncCacheClient) Get(cacheGroupName, key string) (interface{}, error) {
 
 	// Try to find an record with a given ID object in Redis
